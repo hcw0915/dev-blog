287db6de110f2f737659d6039ded96434fb1c78b
hcw0915 <love233031@gmail.com>
2025-03-19 18:10:10 +0800

    style: md code syntax highlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [sitemap(), react(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: 'dark-plus'
+      theme: 'aurora-x'
     },
     extendDefaultPlugins: true
   }
